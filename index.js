550223a1cd2762eb97c4441cb5ee1c49485afa0f
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-10-14 11:00:58 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// create repo in GitHub called `temperature-converter`
+// initialize git project locally
+// link and send the project to GitHub
+
+// create a prompt for the user asking them to enter a Fahrenheit temp and let them know we'll convert it to Celsius
+
+// create a function called `convertToCelsius`
+// convertToCelsius takes in the user input
+// use the formula to convert the user input from Fahrenheit to Celsius
+// return the Celsius temp
+
+// create a function called `describeTemperature`
+// describeTemperature takes in a fahrenheit temp
+// if the fahr temp is less than 32
+  // then return `very cold`
+// if the fahr temp is less than 64
+  // then return `cold`
+// if the fahr temp is less than 86
+  // then return `warm`
+// if the fahr temp is less than 100
+  // then return `hot`
+// if the fahr temp is more than or equal 100
+  // then return `very hot`
+
+// alert the user `You entered {fahrenheit temp} which converts to {celsius temp} and that feels {feeling description}`
+
+// push the project to GitHub
